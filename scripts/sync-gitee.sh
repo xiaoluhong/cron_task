c50464f5def1ec3fd0c2d6f563d2c0022e7d3ef3
XiaoluHong <xiaoluhong@rancher.com>
2018-11-18 12:41:39 +0800

    no message

--- scripts/sync-gitee.sh
+++ scripts/sync-gitee.sh
@@ -17,3 +17,11 @@ sync_repo ()
 }
 
 sync_repo
+
+
+git clone https://github.com/xiaoluhong/k8s-website.git
+cd k8s-website
+touch $(date +%Y-%m-%d)
+git add .
+git commit -m "update"
+git push -f https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@xiaoluhong/k8s-website.git
