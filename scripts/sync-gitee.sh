941c4d0a18539c6d046c47f68b64f4cefa04c2dd
XiaoluHong <xiaoluhong@rancher.com>
2018-11-18 13:20:04 +0800

    no message

--- scripts/sync-gitee.sh
+++ scripts/sync-gitee.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
-export repo_list=(rancher rke ui docs os charts rancher-catalog community-catalog server-chart nstall-docker)
+export repo_list="rancher rke ui docs os charts rancher-catalog community-catalog server-chart nstall-docker"
 export github_repo_url=github.com/rancher
 export gitee_repo_url=gitee.com/rancher
 
 sync_repo ()
 {
-    for repo in ${repo_list};
+    for repo in $repo_list;
     do
         cd
         git clone https://${github_repo_url}/${repo}.git
