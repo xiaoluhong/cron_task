a60f15618019afd6456156de7c297148c1361eb9
XiaoluHong <xiaoluhong@rancher.com>
2018-11-18 13:23:35 +0800

    no message

--- scripts/sync-gitee.sh
+++ scripts/sync-gitee.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-export repo_list="rancher rke ui docs os charts rancher-catalog community-catalog server-chart nstall-docker"
+export repo_list="rancher rke ui docs os charts rancher-catalog community-catalog server-chart install-docker"
 export github_repo_url=github.com/rancher
 export gitee_repo_url=gitee.com/rancher
 
@@ -19,3 +19,10 @@ sync_repo ()
 }
 
 sync_repo
+
+git clone https://github.com/xiaoluhong/k8s-website.git
+cd k8s-website
+touch $(date +%Y-%m-%d)
+git add .
+git commit -m "update"
+git push -f https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@xiaoluhong/k8s-website.git
