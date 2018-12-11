45b88f401faebf38a71bcb7fc7e8255c4c0963bb
XiaoluHong <xiaoluhong@rancher.com>
2018-12-11 14:40:40 +0800

    no message

--- scripts/sync-k8s-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-git clone https://github.com/xiaoluhong/k8s-website.git
-cd k8s-website
-touch $(date +%Y-%m-%d)
-git add .
-git commit -m "update"
-git push -f https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@xiaoluhong/k8s-website.git
