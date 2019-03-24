249a92c47a98429dcb0528e68a33aa49d1d6153c
XiaoluHong <xiaoluhong@rancher.com>
2019-03-24 18:39:42 +0800

    no message

--- scripts/sync-gitee.sh
+++ scripts/sync-gitee.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-export repo_list="rancher rke ui docs os charts rancher-catalog community-catalog server-chart install-docker rio rke-tools longhorn"
+export repo_list="rancher rke ui docs os charts rancher-catalog community-catalog server-chart install-docker rio rke-tools longhorn k3s "
 export github_repo_url=github.com/rancher
 export gitee_repo_url=gitee.com/rancher
 
-sync_repo ()
+sync_repo_github_gitee_rancher ()
 {
     for repo in $repo_list;
     do
@@ -18,7 +18,15 @@ sync_repo ()
 
 }
 
-sync_repo
+sync_repo_github_gitee_rancher
+
+git clone https://github.com/xiaoluhong/rancher-images-localization.git
+cd rancher-images-localization
+git add .
+git commit -m "update"
+git push -f https://${GITEE_ACC}:${GITEE_PW}@gitee.com/hongxl/rancher-images-localization.git
+            
+cd && rm -rf *
 
 git clone https://github.com/xiaoluhong/k8s-website.git
 cd k8s-website
