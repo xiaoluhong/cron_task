b5aa2f2a7fb5a3ef059152a372bd719f41e2b089
Xiaolu Hong <xiaoluhong@rancher.com>
2020-05-08 12:25:16 +0800

    Update sync-gitee.sh

--- scripts/sync-gitee.sh
+++ scripts/sync-gitee.sh
@@ -12,7 +12,7 @@ sync_repo_github_gitee_rancher ()
         git clone https://${github_repo_url}/${repo}.git
         cd ${repo}
         touch update.txt
-        echo $(date +%Y-%m-%d) > update.txt
+        echo $(date +%Y-%m-%d:%H:%M:%S) > update.txt
         git add .
         git commit -m "update"
         git push -f https://${GITEE_ACC}:${GITEE_PW}@${gitee_repo_url}/${repo}.git
@@ -27,7 +27,7 @@ sync_repo_github_gitee_rancher
 git clone https://github.com/xiaoluhong/rancher-images-localization.git
 cd rancher-images-localization
 touch update.txt
-echo $(date +%Y-%m-%d) > update.txt
+echo $(date +%Y-%m-%d:%H:%M:%S) > update.txt
 git add .
 git commit -m "update"
 git push -f https://${GITEE_ACC}:${GITEE_PW}@gitee.com/hongxl/rancher-images-localization.git      
@@ -36,7 +36,7 @@ cd && rm -rf *
 git clone https://github.com/xiaoluhong/k8s-website.git
 cd k8s-website
 touch update.txt
-echo $(date +%Y-%m-%d) > update.txt
+echo $(date +%Y-%m-%d:%H:%M:%S) > update.txt
 git add .
 git commit -m "update"
 git push -f https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/xiaoluhong/k8s-website.git
@@ -45,7 +45,7 @@ cd && rm -rf *
 git clone https://github.com/xiaoluhong/ansible.git
 cd ansible
 touch update.txt
-echo $(date +%Y-%m-%d) > update.txt
+echo $(date +%Y-%m-%d:%H:%M:%S) > update.txt
 git add .
 git commit -m "update"
 git push -f https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/xiaoluhong/ansible.git
@@ -54,7 +54,7 @@ cd && rm -rf *
 git clone -b file-download https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/cnrancher/website.git
 cd website
 touch update.txt
-echo $(date +%Y-%m-%d) > update.txt
+echo $(date +%Y-%m-%d:%H:%M:%S) > update.txt
 git add .
 git commit -m "update"
 git push -f https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/cnrancher/website.git
@@ -63,7 +63,7 @@ cd && rm -rf *
 git clone https://github.com/cnrancher/runlike.git
 cd runlike
 touch update.txt
-echo $(date +%Y-%m-%d) > update.txt
+echo $(date +%Y-%m-%d:%H:%M:%S) > update.txt
 git add .
 git commit -m "update"
 git push -f https://${GITHUB_USERNAME}:${GITHUB_PASSWORD}@github.com/cnrancher/runlike.git
@@ -72,7 +72,7 @@ cd && rm -rf *
 git clone https://github.com/xiaoluhong/server-chart.git
 cd server-chart
 touch update.txt
-echo $(date +%Y-%m-%d) > update.txt
+echo $(date +%Y-%m-%d:%H:%M:%S) > update.txt
 git add .
 git commit -m "update"
 git push -f https://${GITEE_ACC}:${GITEE_PW}@${gitee_repo_url}/server-chart.git
